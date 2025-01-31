11d22d072de919b483deaa644e47c38b3f52b09e
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2025-01-31 13:20:18 -0500

    Renamed folders to reflect project name

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "Tmp",
+    name: "Gol",
     products: [
         .executable(
-            name: "Tmp",
-            targets: ["Tmp"])
+            name: "Gol",
+            targets: ["Gol"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Tmp",
+            name: "Gol",
             dependencies: [],
             resources: [.process("default.metallib")]),
         .testTarget(
-            name: "TmpTests",
-            dependencies: ["Tmp"]
+            name: "GolTests",
+            dependencies: ["Gol"]
         ),
     ]
 )
